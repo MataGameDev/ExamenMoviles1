aedb34e9fa4e0ae74570bf8c0ec93e78363cfe67
MataGitUniat <mikes0174@hotmail.com>
2022-03-21 11:33:50 -0600

    Css on GuessmyNumber

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,72 +1,108 @@
-import { eventWrapper } from '@testing-library/user-event/dist/utils';
 import React, { Component } from 'react';
-class Game extends Component 
-{
-    constructor()
-    {
+import './Game.css';
+
+class Game extends Component {
+    constructor() {
         super()
-        this.state =
-        {
+        this.state = {
             number: "",
             message: "",
             random: generateRandomNumber(100)
         }
+
+        
     }
 
-    handleOnChange = e =>
-    {
+    handleOnChange = e => {
         //const value = e.target.value
-        const {target:{value}} =e;
+        const {target: {value}} = e;
+
         console.log(value);
-        if(value.trim() > 0)
-        {
-            this.setState({
-                    number:value,
-                });
+
+        //Evito que la página se recargue presionando enter
+        if(e.keyCode === 13) {
+            e.preventDefault();
         }
-        this.setState({
-                number:value,
+
+        if(value.trim() > 0) {
+            this.setState({
+                number: value
             });
+        }
+       
+        /* Message vuelve a su estado inicial para dejar
+         de mostrar el mensaje en pantalla al meter un nuevo */ 
+         this.setState({
+            message: "",
+        });
     }
 
-    handleOnClick = () =>
-    {
+    handleOnClick = () => {
         const number = parseInt(this.state.number);
         const random = parseInt(this.state.random);
-        console.log(this.state.random);
+        const text = calculateText(number, random);
+        console.log(random);
 
-        const text = calculateText(number,random);
-
-        this.setState({
-            message : text
-        })
-
+        /* Determina que si el número es diferente de random devuelve
+        number a su estado inicial 
+        de otro modo cuando ganas quiero que el número 
+        no vuelva a su estado inicial para que en el input se muestre el número con el que ganaste*/
+        if (number !== random){
+            this.setState({
+                number: "",
+                message: text,
+            });
+        } else {
+            this.setState({
+                message: text,
+            });
+        }
+        
     }
-
     render() {
+        
         return (
-            <div className ="Game">
+            <div className="Game">
+                <p>Adivina el número que estoy pensando entre el 1 - 100</p>
                 <input
                     type="number"
                     value = {this.state.number}
                     onChange = {this.handleOnChange}
                 />
                 <button onClick={this.handleOnClick}>Probar</button>
-                <p>{this.state.message}</p>
+                <h2 className={(this.state.message)&& 'flickering'}>{this.state.message}</h2>
             </div>
         );
     }
 }
+
 export default Game;
-function generateRandomNumber(max,min=1)
-{
-    return Math.floor(Math.random()*(max-min) + min);
+
+function generateRandomNumber(max, min=1) {
+    return Math.floor(Math.random()*(max - min) + min);
 }
 
-function calculateText(number,random)
-{
-    if(number === random)
-    {
-        return "Felicidades has acertado el numero!!"
+function calculateText(number, random) {
+    const soClose = 5;
+    const diff = Math.abs(random - number);
+    
+    if (number === random) {
+        return "Felicidades, lo has adivinado!!";
+    }
+    
+    if (diff < soClose) {
+        
+        if (number < random) {
+            return "Estás muy cerca!! tu número es un poco bajo."
+        } else {
+            return "Estás muy cerca!! tu número es un poco alto."
+        }
+
+    } else {
+        if (number < random) {
+            return "Tu número es muy bajo!"
+        } else {
+            return "Tu número es muy alto!"
+        }
     }
 }
